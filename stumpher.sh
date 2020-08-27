21a2a4ae79a34de1b6e910856d8cf034f3597cf3
Your Name <you@example.com>
2020-08-27 22:06:58 +0200

    tryfix for short clips

--- stumpher.sh
+++ stumpher.sh
@@ -104,8 +104,8 @@ while [[ $j -lt $NUM ]]; do
     # to overwrite the image
     sec=$(( ($LEN_SECONDS / $NUM) * $j ))
 	  co=$(( $co + 1 ))
-	  OF=$( printf "$D/tn-%06d.tif" $sec )
-	  ${FF} -noaccurate_seek -n -ss "${sec}" -i "$UU" -frames:v 1 \
+	  OF=$( printf "$D/tn-%06d.tif" $j )
+	  ${FF} -noaccurate_seek -ss "${sec}" -i "$UU" -frames:v 1 \
 		-vf scale=iw/$besides:ih/$besides $OF 
 	  S2=$sec
 	  H=0
